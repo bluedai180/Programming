de2acdd5e4783baf612f406fe5419d9ffd988034
blue <bluedai180@gmail.com>
2017-09-04 17:37:58 +0800

    Add KMP.java

---
+++ src/chapter5/KMP.java
@@ -0,0 +1,43 @@
+package chapter5;
+
+/**
+ * Created by Blue on 2017/9/4.
+ */
+public class KMP {
+    private String pat;
+    private int[][] dfa;
+
+    public KMP(String pat) {
+        this.pat = pat;
+        int M = pat.length();
+        int R = 256;
+        dfa = new int[R][M];
+        dfa[pat.charAt(0)][0] = 1;
+        for (int X = 0, j = 1; j < M; j++) {
+            for (int c = 0; c < R; c++) {
+                dfa[c][j] = dfa[c][X];
+            }
+            dfa[pat.charAt(j)][j] = j + 1;
+            X = dfa[pat.charAt(j)][X];
+        }
+    }
+
+    public int search(String txt) {
+        int i, j, N = txt.length(), M = pat.length();
+        for (i = 0, j = 0; i < N && j < M; i++)
+            j = dfa[txt.charAt(i)][j];
+        if (j == M) return i - M;
+        else return N;
+    }
+
+    public static void main(String[] args){
+        String pat = "ABABAC";
+        String txt = "BCBAABACAABABACAA";
+        KMP kmp = new KMP(pat);
+        System.out.println(txt);
+        int offset = kmp.search(txt);
+        for (int i=0;i<offset;i++)
+            System.out.print(" ");
+        System.out.println(pat);
+    }
+}
